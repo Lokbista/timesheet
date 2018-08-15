47dc5083ca178a2882f8025093bfc41a9c4c61f7
JY7GK0 <lok.b.bistkshetri@usps.gov>
2018-08-15 18:13:37 -0400

    jpa connection added

--- src/main/java/com/lok/timesheet/spring/controller/TimesheetEntryController.java
+++ src/main/java/com/lok/timesheet/spring/controller/TimesheetEntryController.java
@@ -2,12 +2,24 @@ package com.lok.timesheet.spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lok.timesheet.spring.form.SignInForm;
 
 @Controller
 public class TimesheetEntryController {
 
 	@GetMapping(value ="/enter-timesheet")
 	public String enterTimesheet() {
+		System.out.println("go to sign in");
 		return "/timesheet/timesheet-entry.html";
+	}
+	@RequestMapping("/start-shift")
+	public String startYourShift(@ModelAttribute("midProfileForm") SignInForm signInForm)
+	 {
+		System.out.println("start your shift");
+		return "/timesheet/timesheet-success.html";
 	}
 }
